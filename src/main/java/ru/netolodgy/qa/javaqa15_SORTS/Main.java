cfb5d823acdc77f65c4c1b78d6a88cd5b51e48ed
Konysheva Tatyana <tkonyseva693@gmail.com>
2023-05-25 14:34:27 +0500

    Correctioan

--- src/main/java/ru/netolodgy/qa/javaqa15_SORTS/Main.java
+++ src/main/java/ru/netolodgy/qa/javaqa15_SORTS/Main.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Ticket ticket1 = new Ticket("Moscow", "Tyumen",10_000,6,8,2);
-        Ticket ticket2 = new Ticket("Moscow", "Surgut", 20_000,7,10,3);
+        Ticket ticket1 = new Ticket("Moscow", "Tyumen", 10_000, 6, 8, 2);
+        Ticket ticket2 = new Ticket("Moscow", "Surgut", 20_000, 7, 10, 3);
 
         System.out.println(ticket1.compareTo(ticket2));
         System.out.println(ticket2.compareTo(ticket1));
@@ -15,11 +15,4 @@ public class Main {
 
     }
 
-
-//
-//        BookPriceComparator priceComparator = new BookPriceComparator();
-//
-//        Book[] books = { book1, book2 };
-//        Arrays.sort(books, priceComparator);
-//    }
 }
